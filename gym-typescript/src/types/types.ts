367f7a5411240ddcd2dacef00dedbb95306891bf
Yaroslav Yaroshovets <white240403@gmail.com>
2023-03-19 16:54:42 +0200

    classes page added

--- gym-typescript/src/types/types.ts
+++ gym-typescript/src/types/types.ts
@@ -5,9 +5,16 @@ export enum SelectedPage {
   ContactUs = "contactus",
 }
 
-export interface IBenefitType {
+export interface IBenefit {
   id: string;
   icon: JSX.Element;
   title: string;
   description: string;
+}
+
+export interface IClass {
+  id: string;
+  name: string;
+  description?: string;
+  image: string;
 }
